992929c52105394ae383a0f02d541737397ebc80
Santhiya Chakravarthi <santhiya@Santhiyas-MacBook-Pro.local>
2026-04-11 22:50:17 -0700

    Titanic ML project with Streamlit + SHAP

---
+++ day42_2_Titanic_FastTrack_2.py
@@ -0,0 +1,247 @@
+#  Goal
+
+# Turn raw Titanic data into smart features that improve prediction
+
+# Why Feature Engineering Matters
+# A model doesn’t “understand” data like humans.
+# We convert raw columns into meaningful signals.
+
+# Example:
+
+# Name  → useless as-is
+# Extract Title  → VERY powerful
+# Dataset Columns (Quick Recall)
+# PassengerId
+# Survived (target)
+# Pclass
+# Name
+# Sex
+# Age
+# SibSp
+# Parch
+# Ticket
+# Fare
+# Cabin
+# Embarked
+
+
+# What Each Column Means (Quick Clarity)
+
+# Let’s simplify your columns:
+
+# PassengerId → just ID (we will drop later)
+# Survived →  target (0 = No, 1 = Yes)
+# Pclass → ticket class (rich/poor)
+# Name → useful for feature engineering (Title)
+# Sex → gender (important)
+# Age → has missing values
+# SibSp → siblings/spouse
+# Parch → parents/children
+# Ticket → mostly useless
+# Fare → ticket price
+# Cabin → many missing values
+# Embarked → port (categorical)
+
+
+import pandas as pd
+
+
+df = pd.read_csv('titanic.csv')
+
+# 🚀 Now We Move Forward (IMPORTANT STEP)
+
+# Before feature engineering, we must understand the data
+
+print("Before Handling Misisng values, Feature Engineering and Encode df.head:\n",df.head())
+print("Before Handling Misisng values, Feature Engineering and Encode df.shape\n:",df.shape)
+# df.info()
+print("df.isnull().sum()\n:",df.isnull().sum())
+
+# output:
+# df.isnull().sum(): PassengerId      0
+# Survived         0
+# Pclass           0
+# Name             0
+# Sex              0
+# Age            177
+# SibSp            0
+# Parch            0
+# Ticket           0
+# Fare             0
+# Cabin          687
+# Embarked         2
+
+#  Understand Missing Data (Very Important)
+
+# Age → 177 missing (~20%)
+# Cabin → 687 missing (~77%) ❗
+# Embarked → 2 missing (very small)
+
+# 👉 Decision thinking (this is what interviewers expect):
+# | Column   | Action            | Reason            |
+# | -------- | ----------------- | ----------------- |
+# | Age      | Fill              | Important feature |
+# | Cabin    | Drop or transform | Too many missing  |
+# | Embarked | Fill              | Very few missing  |
+
+#  Step 2: Handle Missing Values
+#  we can handle misisng values using inmputer in pipeline
+
+# Handle Cabin
+# Too many missing → drop
+df.drop('Cabin', axis=1, inplace=True)
+
+
+
+# Feature Engineering (GAME CHANGER)
+
+# Now we create powerful features 
+
+# Create Family Size
+# Why?
+# People with family had higher survival chances
+df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+
+
+# Extract Title from Name 
+# 👉 Example:
+# "Mr", "Mrs", "Miss", "Master"
+df['Title'] = df['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+
+#  Simplify Titles
+                               
+df['Title'] = df['Title'].replace({
+    'Mlle': 'Miss',
+    'Ms': 'Miss',
+    'Mme': 'Mrs',
+    'the Countess': 'Countess'   # 👈 Fix added
+})
+df['Title'] = df['Title'].replace(
+    ['Lady', 'Countess','Capt','Col','Don','Dr','Major','Rev','Sir','Jonkheer','Dona'],
+    'Rare'
+)
+print(df['Title'].unique())
+
+#  Drop Useless Columns
+# No predictive value (noise)
+df.drop(['Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
+
+# IsAlone based on Family Size:
+df['IsAlone'] = (df['FamilySize'] == 1).astype(int)
+print(df['IsAlone'].unique())
+
+################################
+
+print("df['Title'].isnull().sum():\n",df['Title'].isnull().sum())
+
+
+# Split Data for Train and Test
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("#########################  Started ColumnTransformer In pipeline with XGBoost")
+
+# Titanic Features Setup
+# Example features:
+num_features = ['Age', 'Fare']
+cat_features = ['Sex', 'Embarked', 'Pclass']
+
+# Import Required Libraries
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from sklearn.impute import SimpleImputer
+import joblib
+from sklearn.metrics import accuracy_score
+
+# Build Preprocessing Pipeline
+# Numeric Pipeline
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')),   # Handing Missing values
+    ('scaler', StandardScaler())
+])
+# Categorical Pipeline
+cat_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='most_frequent')), # Handing Missing values
+    ('encoder', OneHotEncoder(handle_unknown='ignore'))
+])
+#ColumnTransformer
+preprocessor = ColumnTransformer([
+    ('num', num_pipeline, num_features),
+    ('cat', cat_pipeline, cat_features)
+])
+# Full Pipeline 
+pipeline_XGB = Pipeline([
+    ('preprocessing', preprocessor),
+    # ('smote', SMOTE(random_state=42)),
+    ('model', XGBClassifier(random_state=42,))
+])
+
+#Key change: use model__ prefix
+param_grid = {
+    'model__n_estimators': [100],
+    'model__max_depth': [4],
+    'model__learning_rate': [0.1]
+}
+
+# 🔧 GridSearch with Pipeline
+grid_pipeline_XGB = GridSearchCV(
+    pipeline_XGB,
+    param_grid,
+    cv=2,
+    verbose=1,
+    n_jobs=1,
+    error_score='raise'
+)
+
+
+print("df['Sex'].unique()\n",df['Sex'].unique())
+print("df['Embarked'].unique():\n",df['Embarked'].unique())
+
+# ▶️ Step 7: Train Model
+print("Started training")
+grid_pipeline_XGB.fit(X_train, y_train)
+joblib.dump(grid_pipeline_XGB,"titanic_pipeline_v1.pkl")
+print("titanic_pipeline_v1.pkl saved successfully")
+grid_pipeline_XGB = joblib.load("titanic_pipeline_v1.pkl")
+print("Best_Params_grid_pipeline_XGB:", grid_pipeline_XGB.best_params_)
+print("Best_Score_grid_pipeline_XGB:", grid_pipeline_XGB.best_score_)
+#Evaluation
+
+
+# Accuracy - grid
+train_accuracy_grid_pipeline_XGB = grid_pipeline_XGB.score(X_train, y_train)
+test_accuracy_grid_pipeline_XGB = grid_pipeline_XGB.score(X_test, y_test)
+print("train_accuracy_grid_pipeline_XGB:", train_accuracy_grid_pipeline_XGB)
+print("test_accuracy_grid_pipeline_XGB:", test_accuracy_grid_pipeline_XGB) 
+
+# 📊 Step 8: Predict
+y_pred_grid_pipeline_XGB = grid_pipeline_XGB.predict(X_test)
+
+# ✅ Step 9: Accuracy
+print("Accuracy_grid_pipeline_XGB:", accuracy_score(y_test, y_pred_grid_pipeline_XGB))
+
+# ✅ Step 3: Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm_grid_pipeline_XGB = confusion_matrix(y_test, y_pred_grid_pipeline_XGB)
+print("Confusion Matrix_grid_pipeline_XGB:\n", cm_grid_pipeline_XGB)
+
+# ✅ Step 4: Full Report
+from sklearn.metrics import classification_report
+cr_grid_pipeline_XGB = classification_report(y_test, y_pred_grid_pipeline_XGB)
+print('classification_report_grid_pipeline_XGB:\n',cr_grid_pipeline_XGB)
+
+
+
+
+print("#########################  Completed ColumnTransformer In pipeline with XGBoost")
+
+# Production-ready
+
---
+++ titanic_streamlit_app.py
@@ -0,0 +1,253 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+print("Start the streamlie******")
+def clean_feature_name(name, value, input_data):
+    print("clean_feature_name:Name:\n",name)
+    print("clean_feature_name:Value:\n",value)
+    name = name.replace("cat__", "").replace("num__", "")
+
+    # 🎯 Numeric features → show original value
+    if name in ["Age", "Fare"]:
+     original_value = input_data[name].iloc[0]
+     if name == "Age":
+        if original_value < 13:
+            return f"Child (Age {round(original_value, 2)})"
+        elif original_value < 20:
+            return f"Teen (Age {round(original_value, 2)})"
+        elif original_value < 60:
+            return f"Adult (Age {round(original_value, 2)})"
+        else:
+            return f"Senior (Age {round(original_value, 2)})"
+
+     return f"{name} ({round(original_value, 2)})"
+
+    # 🎯 Sex
+    if "Sex_female" in name:
+        return "Female" if value == 1 else "Male"
+
+    if "Sex_male" in name:
+        return "Male" if value == 1 else "Female"
+
+    # 🎯 Pclass
+    if "Pclass" in name and value == 1:
+        return f"Class {name.split('_')[-1]}"
+
+    # 🎯 Embarked
+    if "Embarked" in name and value == 1:
+        return f"Embarked {name.split('_')[-1]}"
+
+    return None  # skip inactive features
+
+
+@st.cache_resource
+def load_model():
+    print("Loading model only once...")
+    
+    grid = joblib.load("titanic_pipeline_v1.pkl")
+    pipeline = grid.best_estimator_
+
+
+    preprocessor = pipeline.named_steps['preprocessing']
+    print("preprocessor:\n",preprocessor)
+    model = pipeline.named_steps['model']
+    encoder_cat = preprocessor.named_transformers_['cat'].named_steps['encoder']
+    print("Encoder_Categories:", encoder_cat.categories_)
+    scaler = preprocessor.named_transformers_['num'].named_steps['scaler']
+    print("scaler:",scaler)
+    print("Mean:", scaler.mean_)
+    print("Scale:", scaler.scale_)
+    feature_names = preprocessor.get_feature_names_out()
+    print(" loading model : feature_names",feature_names)
+    clean_names = [name.split("__")[1] for name in feature_names]
+    print(" loading model : clean_names",clean_names)
+    return pipeline, preprocessor, model, clean_names
+
+
+
+pipeline, preprocessor, model, clean_names = load_model()
+
+@st.cache_resource(hash_funcs={type(model): id})
+def get_explainer(model):
+    print("Creating SHAP explainer once...")
+    return shap.TreeExplainer(model)
+
+explainer = get_explainer(model)
+print("explainer:\n",explainer)
+
+def get_feature_emoji(name):
+    if "Age" in name or "Child" in name or "Adult" in name:
+        return "👶"
+    if "Fare" in name:
+        return "💰"
+    if "Male" in name:
+        return "👨"
+    if "Female" in name:
+        return "👩"
+    if "Class" in name:
+        return "🎟️"
+    if "Embarked" in name:
+        return "🛳️"
+    return "🔹"
+
+THRESHOLD = 0.5
+
+st.title("🚢 Titanic Survival Prediction App")
+
+st.write("Adjust passenger details to predict survival probability")
+
+# 🎛️ Inputs
+pclass = st.selectbox("Passenger Class", [3, 2, 1])
+sex = st.selectbox("Sex", ["male", "female"])
+age = st.slider("Age", 0, 80, 30)
+fare = st.slider("Fare", 0.0, 300.0, 55.0)
+embarked = st.selectbox("Embarked", ["Q", "C", "S"])
+
+# 📦 Create DataFrame
+input_data = pd.DataFrame([{
+    "Pclass": pclass,
+    "Sex": sex,
+    "Age": age,
+    "Fare": fare,
+    "Embarked": embarked
+}])
+
+
+
+if pclass == 3 and fare > 40:
+    st.warning("⚠️ Unusual combination: High fare for 3rd class")
+
+if pclass == 1 and fare < 20:
+    st.warning("⚠️ Unusual: Very low fare for 1st class")
+
+# 🔮 Prediction
+if st.button("Predict"):
+    # ✅ Transform input first
+    print("Transforming input")
+    print("input_data:\n",input_data)
+    input_data["Sex"] = input_data["Sex"].str.lower()
+    input_data["Embarked"] = input_data["Embarked"].str.upper()
+    print("input_data_afterUC_LC:\n",input_data)
+    X_transformed = preprocessor.transform(input_data)
+    print("X_transformed:\n",X_transformed)
+    X_transformed_df = pd.DataFrame(X_transformed,columns=clean_names)
+    print("X_transformed_df:\n",X_transformed_df)
+    
+
+    # ✅ Prediction
+    print("pipeline.predict_proba(input_data)\n",pipeline.predict_proba(input_data))
+    prob = pipeline.predict_proba(input_data)[0][1]
+    print("prob:\n",prob)
+    pred = int(prob >= THRESHOLD)
+    # st.write("Transformed Data:", X_transformed_df)
+    st.subheader("Result")
+    st.write(f"Survival Probability: **{prob:.2f}**")
+
+    if prob > 0.7:
+        st.success("High confidence prediction")
+    elif prob < 0.3:
+        st.info("Low survival probability")
+    else:
+        st.warning("⚠️ Model uncertain")
+
+    if pred == 1:
+        st.success("✅ Likely to Survive")
+    else:
+        st.error("❌ Not Likely to Survive")
+
+    # 🧠 SHAP EXPLANATION
+    st.subheader("🔍 Why this prediction? (SHAP)")
+
+
+    shap_values = explainer(X_transformed_df)
+    values = shap_values.values[0]
+    feature_names = X_transformed_df.columns
+
+    feature_impact = list(zip(feature_names, values))
+# Sort by absolute importance
+    feature_impact = sorted(feature_impact, key=lambda x: abs(x[1]), reverse=True)
+    st.markdown("### 🧠 Top Factors Influencing Prediction")
+    st.markdown("---")
+    print("feature_impactAll:\n",feature_impact)
+    print("feature_impact.shape:",np.array(feature_impact).shape)
+    filtered_features = []
+    for name, val in feature_impact:
+      value = X_transformed_df[name].iloc[0]
+      print("name :\n",name)
+      print("val :\n",val)
+      print("value:\n",value)
+      # Skip inactive one-hot features
+      if ("Sex_" in name or "Pclass_" in name or "Embarked_" in name) and value == 0:
+        continue
+      clean_name = clean_feature_name(name,value,input_data)
+      if clean_name is not None:
+        filtered_features.append((clean_name, val))
+
+      print("filtered_features:\n",filtered_features)
+
+    # 👉 Take top 3
+    top_features = sorted(filtered_features, key=lambda x: abs(x[1]), reverse=True)[:3]
+
+    print("top_features:\n",top_features)
+
+    # 👉 Display
+    for name, val in top_features:
+        emoji = get_feature_emoji(name)
+
+        strength = abs(val)
+
+        if val > 0:
+            if strength > 1:
+                st.success(f"{emoji} **{name}** strongly increased survival")
+            else:
+                st.write(f"{emoji} ✅ **{name}** → increased survival probability")
+        else:
+            if strength > 1:
+                st.error(f"{emoji} **{name}** strongly decreased survival")
+            else:
+                st.write(f"{emoji} ❌ **{name}** → decreased survival probability")
+
+    # Plot
+    fig, ax = plt.subplots()
+    shap.plots.waterfall(
+        shap_values[0],
+        max_display=10,
+        show=False
+    )
+    st.pyplot(fig)
+
+
+
+# We’ll convert SHAP into plain English explanations
+
+# 🧠 What We’re Building
+
+# Instead of only chart:
+
+# 👉 You’ll show:
+
+# Top factors influencing prediction:
+
+# 1. Female → increased survival probability
+# 2. Age → decreased survival probability
+# 3. Fare → decreased survival probability
+
+
+# 🚀 Step-by-Step Implementation
+# ✅ Step 1: Clean Feature Names
+
+
+# ✅ Step 2: Extract SHAP Values
+
+# After:
+# shap_values = explainer(X_transformed_df)
+
+# Add:
+
+# values = shap_values.values[0]
+# feature_names = X_transformed_df.columns
